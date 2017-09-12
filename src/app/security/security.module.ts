3acebdad46096875f725d02b17561d22f45193ca
Gregor Woiwode <gw@co-it.eu>
2017-09-12 22:40:02 +0200

    create component for login

--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MdCardModule } from '@angular/material';
 
 import { SecurityRoutingModule } from './security-routing.module';
+
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule,
+
+    MdCardModule,
+
     SecurityRoutingModule
   ],
-  declarations: []
+  declarations: [LoginComponent]
 })
 export class SecurityModule { }
