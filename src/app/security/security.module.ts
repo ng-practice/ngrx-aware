fe72b0e45c2c54d44df38ed8f418865fdc677d61
Gregor Woiwode <gw@co-it.eu>
2017-09-12 22:06:50 +0200

    (new) add module security handling login and registration

---
+++ src/app/security/security.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SecurityRoutingModule } from './security-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SecurityRoutingModule
+  ],
+  declarations: []
+})
+export class SecurityModule { }
