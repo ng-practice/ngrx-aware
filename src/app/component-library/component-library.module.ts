6abcf87fbb3fd4641b6232e9adf20da20f0bdc8a
Gregor Woiwode <gw@co-it.eu>
2017-09-15 16:29:54 +0200

    (new) add library for shared components

---
+++ src/app/component-library/component-library.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessageModal } from './message-modal/message-modal.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MessageModal]
+})
+export class ComponentLibraryModule { }
---
+++ src/app/component-library/message-modal/message-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'awr-message-modal',
+  templateUrl: './message-modal.component.html',
+  styleUrls: ['./message-modal.component.css']
+})
+export class MessageModal implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
